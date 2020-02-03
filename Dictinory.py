f3e3c19ff713b9b1f7a47f0966124d2c5fbc8d80
Eslam Mohammed <sayko117@gmail.com>
2020-02-03 21:08:52 +0200

    Sensitivity for lower and additional characters

--- Dictinory.py
+++ Dictinory.py
@@ -1,6 +1,12 @@
 import json
 data = json.load(open("data.json"))
 def key_to_def(word):
+	word = word.lower()
+	q=""
+	for q in word:
+		if q in data:
+			if input("Did you mean %s ?" % q) == y)
+				word = q		
 	if data.get(word):
 		a = ""
 		answer = data.get(word)
@@ -8,6 +14,6 @@ def key_to_def(word):
 			a += i + "\n"
 		return a
 	else:
-			return "Error 404 not found"
+		return "Error 404 not found"
 print(key_to_def(input("Please enter: ")))
 
