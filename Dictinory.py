53f426a1229d58bf185adf1459bca94040500a4b
root <root@Devos.localdomain>
2020-02-04 03:34:08 +0200

    Final Touch

--- Dictinory.py
+++ Dictinory.py
@@ -1,21 +1,31 @@
 import json
+from difflib import get_close_matches
 data = json.load(open("data.json"))
 def key_to_def(word):
-	word = word.lower()
-	g="" 
+	word = word.lower() 
 	if data.get(word):
 		a = ""
 		answer = data.get(word)
 		for i in answer:
 			a += i + "\n"
 		return a
+	elif len(get_close_matches(word,data.keys())) > 0:
+		h = input("Did you mean %s instead? (y or n)" % get_close_matches(word,data.keys())[0])
+		if h == "y":
+			return key_to_def(get_close_matches(word,data.keys())[0])
+		elif h == "n":
+			return "Error 404 not found"
+		else:
+			return "Sorry we couldn`t understand your answer, please try again"
 	else:
-		for q in word:
-			g+=q
-			if g in data:
-				t = "Did you mean %s ? (y or n)" % g
-				if input(t) == "y":
-					return key_to_def(g)
 		return "Error 404 not found"
 print(key_to_def(input("Please enter: ")))
 
+
+#g=""
+		#for q in word:
+			#g+=q
+			#if g in data:
+				#t = "Did you mean %s ? (y or n)" % g
+				#if input(t) == "y":
+					#return key_to_def(g)
