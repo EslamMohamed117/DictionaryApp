59775c7a48f7b5d28df3240c08d1923f33ba44c3
root <root@Devos.localdomain>
2020-02-04 02:10:11 +0200

    Sensitivity fixed

--- Dictinory.py
+++ Dictinory.py
@@ -2,7 +2,7 @@ import json
 data = json.load(open("data.json"))
 def key_to_def(word):
 	word = word.lower()
-	q="" 
+	g="" 
 	if data.get(word):
 		a = ""
 		answer = data.get(word)
@@ -11,15 +11,11 @@ def key_to_def(word):
 		return a
 	else:
 		for q in word:
-			if q in data:
-				t = "Did you mean %s ?" % q
-				print(t)
+			g+=q
+			if g in data:
+				t = "Did you mean %s ? (y or n)" % g
 				if input(t) == "y":
-					return key_to_def(q)
-                        	else:
-                                	q+=q
-			else:
-				q+=q
-			return "Error 404 not found"
+					return key_to_def(g)
+		return "Error 404 not found"
 print(key_to_def(input("Please enter: ")))
 
